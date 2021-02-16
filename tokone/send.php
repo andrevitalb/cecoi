2a6e0040c13cb12195267ee107035d0ee3453885
André Vital <santiago_vital@outlook.com>
2021-02-16 10:15:30 -0600

    Added base & email sending files

---
+++ tokone/send.php
@@ -0,0 +1,29 @@
+﻿    
+               	<?php
+               	    $nombre = $_REQUEST['inputName'];
+               	    $mail = $_REQUEST['inputEmail'];
+               	    $asunto = $_REQUEST['inputAsunto'];
+               	    $textarea = $_REQUEST['message'];
+
+               	    $spam = $_REQUEST['valid_spam'];
+						
+
+					$header = 'De: ' . $mail . " \r\n";
+
+					$mensaje = "Nombre: " . $nombre . " \r\n";
+					$mensaje .= "Mail: " . $mail . " \r\n";
+					$mensaje .= "Asunto: " . $asunto . " \r\n";
+					$mensaje .= "Mensaje: " . $textarea . " \r\n";
+					
+					$mensaje .= "\n\nEnviado el ".date('d-m-Y H:i:s');
+
+					$para = 'ventas@tokone.com.mx';
+					$asunto = 'Contacto Página Web';
+
+					if($spam == false && mail($para, $asunto, $mensaje, $header)) {
+						echo'<body><link href="https://fonts.googleapis.com/css?family=Titan+One&display=swap" rel="stylesheet"><div><h1>Mensaje Enviado</h1></div><style>body { margin: 0; padding: 0; }div { display: block; text-align: center; background-image: url(assets/images/custom/prods_bg.jpg); background-size: cover; height: 100%; width: 100vw;height: 100vh; }h1{ font-size: 85px; display: inline-block; margin: 24% auto; color: #fff; font-family:  Titan One, cursive; }</style></body><meta http-equiv="refresh" content="1.75;URL=http://www.tokone.com.mx/confirmacion.html" />';
+					}
+					else {
+					  	echo'<body><link href="https://fonts.googleapis.com/css?family=Titan+One&display=swap" rel="stylesheet"><div><h1>Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.</h1></div><style>body{ margin: 0; padding: 0; }div{ display: block; text-align: center; background-image: url(assets/images/custom/prods_bg.jpg); background-size: cover; width: 100vw; height: 100vh }h1{ padding: 3.5rem; background-color: rgba(255,255,255,.5); font-size: 60px; display: inline-block; margin: 19.5% auto; color: #000; font-family:  Titan One, cursive; }</style></body><meta http-equiv="refresh" content="1.75;URL=http://www.tokone.com.mx/"/>';
+					  }					    
+				?>
